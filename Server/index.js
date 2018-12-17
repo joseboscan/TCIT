b86e658e4bca51ae7fee6518212815465ebb9752
joseboscan <45270778+joseboscan@users.noreply.github.com>
2018-12-17 18:46:45 -0300

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -25,37 +25,44 @@ app.use((req, res, next) => {
 
 client.connect();
 
-app.get('/list',(request,response)=>{
+app.get('/list*',(request,response)=>{
 client.query("select * from blogs")
     .then(responseSql => {
-        console.log(responseSql.rows)
-        list=responseSql.rows
-	response.json(list);
+          list=responseSql.rows
+         if(request.query.key==='Ep0ooe44dw9bvxZvW38639MnuHfD91')
+	       response.json(list);
+        else
+          response.json();
   	
     })
     .catch(err => {
         client.end();
         response.json('0');
-        console.log("Error")
+ 
     })
    })
 
 app.get('/remove*',(request,response)=>{
+  if(request.query.key==='Ep0ooe44dw9bvxZvW38639MnuHfD91')
+  {
 var query='DELETE FROM blogs WHERE "Id_Blog" ='+request.query.id
 		console.log(request,'remove',request.query.id,query);
 client.query(query)
     .then(responseSql => {
-			return 1;
+  			return (1);
     })
     .catch(err => {
         client.end();
-        response.json('0');
+        response('0');
+    })
+}else return 0
     })
 
-    })
 
 app.get('/insert*',(request,response)=>{
-
+      console.log(request.query.key)
+if(request.query.key==='Ep0ooe44dw9bvxZvW38639MnuHfD91')
+{
 var query="INSERT INTO blogs(name, description) VALUES ( '"+request.query.nombre+"', '"+request.query.description+"')"
 console.log(response,query);
 client.query(query)
@@ -67,6 +74,7 @@ client.query(query)
         response.json('0');
         console.log("Error")
     })
+  }else return 0
     })
 
 app.listen('3200', function() {
